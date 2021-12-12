bca9508f7bf66ebbd2b2306b1358f86126502856
shn2002 <joe.shanhenian@gmail.com>
2021-12-12 15:45:56 -0500

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+let  http = require('http')
+
+let users = [
+    { id: 1, name: 'Tom' },
+    { id: 2, name: 'Jerry' },
+    { id: 3, name: 'Susan' }
+
+]
+let server = http.createServer(function (req,res) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    if (req.url ==='/api/users'){
+        res.end(JSON.stringify(users));
+    }else{
+        res.end('Not Found');
+    }
+});
+server.listen(3000,()=>{
+    console.log("back end server boot at port 3000");
+})
